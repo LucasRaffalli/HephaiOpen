a3973f720d9998105e42f939c9678da1d11970a6
Lucas <lucas.raffalli17@gmail.com>
2025-05-30 22:46:41 +0200

    fix: simplifier le workflow de construction pour Windows et mettre à jour la version de l'application à 0.0.19

--- vite.config.ts
+++ vite.config.ts
@@ -4,91 +4,102 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
 import pkg from './package.json'
-import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr"
 
-export default defineConfig(({ command }) => {
-  rmSync('dist-electron', { recursive: true, force: true })
-
-  const isServe = command === 'serve'
-  const isBuild = command === 'build'
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
-
-  return {
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src'),
-        '@electron': path.resolve(__dirname, './electron'),
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      '@electron': path.resolve(__dirname, './electron'),
     },
-    base: './',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      minify: 'esbuild',
-      emptyOutDir: true,
-      target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-        },
-        output: {
-          format: 'esm',
-          entryFileNames: '[name].[hash].js',
-          chunkFileNames: '[name].[hash].js',
-          assetFileNames: '[name].[hash].[ext]',
-          manualChunks: {
-            'vendor': [
-              'react', 
-              'react-dom',
-            ],
-            'router': ['react-router-dom'],
-            'ui': ['@radix-ui/themes', '@radix-ui/react-icons'],
-          }
-        }
-      },
-    },
-    plugins: [
-      react({
-        jsxRuntime: 'automatic',
-      }),
-      svgr({
-        svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
-        include: "**/*.svg",
-      }),
-      electron({
-        main: {
-          entry: 'electron/main/index.ts',
-          vite: {
-            build: {
-              outDir: 'dist-electron/main',
-              rollupOptions: {
-                external: Object.keys(pkg.dependencies || {})
-              },
+  },
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+    svgr({
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+    }),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron/main',
+            lib: {
+              entry: path.resolve('electron/main/index.ts'),
+              formats: ['cjs']
+            },
+            rollupOptions: {
+              external: Object.keys(pkg.dependencies || {}),
+              output: {
+                format: 'cjs',
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+              }
             },
           },
         },
-        preload: {
-          input: 'electron/preload/index.ts',
-          vite: {
-            build: {
-              outDir: 'dist-electron/preload',
-              rollupOptions: {
-                external: Object.keys(pkg.dependencies || {})
-              },
+      },
+      preload: {
+        input: 'electron/preload/index.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron/preload',
+            lib: {
+              entry: path.resolve('electron/preload/index.ts'),
+              formats: ['cjs']
+            },
+            rollupOptions: {
+              external: Object.keys(pkg.dependencies || {}),
+              output: {
+                format: 'cjs',
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+              }
             },
           },
         },
-        renderer: {},
-      }),
+      },
+      renderer: {},
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'esbuild',
+    emptyOutDir: true,
+    target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        format: 'esm',
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]',
+        manualChunks: {
+          'vendor': [
+            'react', 
+            'react-dom',
+          ],
+          'router': ['react-router-dom'],
+          'ui': ['@radix-ui/themes', '@radix-ui/react-icons'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      '@radix-ui/themes',
     ],
-    optimizeDeps: {
-      include: [
-        'react',
-        'react-dom',
-        'react-router-dom',
-        '@radix-ui/themes',
-      ],
-      force: true
-    }
-  }
+    force: true
+  },
 })
